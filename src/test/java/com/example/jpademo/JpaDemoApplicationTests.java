52e20d6371624ce0ec01b63e18ffcd814de5258c
houyou <houyou@tallsafe.com>
2024-10-15 11:02:23 +0800

    sss

--- src/test/java/com/example/jpademo/JpaDemoApplicationTests.java
+++ src/test/java/com/example/jpademo/JpaDemoApplicationTests.java
@@ -4,21 +4,13 @@ import com.example.jpademo.entity.REquipment;
 import com.example.jpademo.entity.RFieldMap;
 import com.example.jpademo.entity.Tag;
 import com.example.jpademo.entity.TagId;
-import com.example.jpademo.repository.REquipmentRepository;
 import com.example.jpademo.repository.RFieldMapRepository;
-import com.example.jpademo.repository.TagRepository;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionSynchronizationAdapter;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.time.LocalDateTime;
@@ -34,18 +26,11 @@ class JpaDemoApplicationTests {
     private RFieldMapRepository fieldMapRepository;
 
     @Autowired
-    private REquipmentRepository equipmentRepository;
-
-    @Autowired
-    private TagRepository tagRepository;
-
-    @Autowired
     private EntityManager entityManager;
 
     @Autowired
     private TransactionTemplate transactionTemplate;
 
-    private final JsonMapper mapper = JsonMapper.builder().build();
 
     private final CountDownLatch latch = new CountDownLatch(1);
 
@@ -58,6 +43,7 @@ class JpaDemoApplicationTests {
         int makeSize = 140000;
         //数据库默认最大连接数150 我们调整到140测试数据库压力
         int parallel = 140;
+
 
         ThreadPoolExecutor fixedThreadPool = new ThreadPoolExecutor(parallel,parallel,40, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
         for (int j = 0; j < parallel; j++) {
@@ -83,7 +69,7 @@ class JpaDemoApplicationTests {
                         rEquipment.setRemark("remark");
                         rEquipment.setUpdateTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
                         rEquipment.setAuditStatus(1);
-                        equipmentRepository.save(rEquipment);
+                        entityManager.persist(rEquipment);
                         //补充tag
                         List<Tag> tagList = fieldList.stream()
                                 .map(field->{
@@ -105,12 +91,12 @@ class JpaDemoApplicationTests {
                         stringBuilder.replace(stringBuilder.length() - 1, stringBuilder.length(), "").append(";");
                         entityManager.createNativeQuery(stringBuilder.toString()).executeUpdate();
                         System.out.println(Thread.currentThread().getName().concat("-").concat(String.valueOf(finalI))+"完成插入");
-                        try {
-                            Thread.currentThread().wait(1000*30);
+                     /*   try {
+                            Thread.currentThread().wait();
                         } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
-                        System.out.println(Thread.currentThread().getName().concat("-").concat(String.valueOf(finalI))+"睡醒了");
+                        System.out.println(Thread.currentThread().getName().concat("-").concat(String.valueOf(finalI))+"睡醒了");*/
                         status.flush();
                         return null;
                     });
